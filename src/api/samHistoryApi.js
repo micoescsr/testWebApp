1cc7f05303c821097b69b47c6051102dbd1b94eb
cscendaya <carlieshaye24@gmail.com>
2026-02-10 10:29:53 +0800

    added vuln and threat history from db ; needs to be refined

--- src/api/samHistoryApi.js
+++ src/api/samHistoryApi.js
@@ -1,14 +1,7 @@
 // api/samHistoryApi.js
 import api from "./axios";
 
-// Vulnerability scan history per run / per SSID
-export const getVulnHistory = () => {
-  // Expected backend route: GET /sam/history/vulnerabilities
-  return api.get("/sam/history/vulnerabilities");
-};
 
-// Threat scan history per run / per SSID
-export const getThreatHistory = () => {
-  // Expected backend route: GET /sam/history/threats
-  return api.get("/sam/history/threats");
-};
+// api/samHistoryApi.js
+export const getVulnHistory = () => api.get("/history/vulnerabilities");
+export const getThreatHistory = () => api.get("/history/threats");
