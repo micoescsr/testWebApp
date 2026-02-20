813a12974655d0f23729974bc52bdce2ea459587
cscendaya <carlieshaye24@gmail.com>
2026-02-20 13:30:29 +0800

    ; changed sam.jsx's saveselectednetwork to /api/raspi/networks

--- src/pages/SAM/SAM.jsx
+++ src/pages/SAM/SAM.jsx
@@ -170,7 +170,8 @@ const SAM = () => {
     }
 
     try {
-      const res = await fetch("/api/networks", {
+      //const res = await fetch("/api/networks", {
+      const res = await fetch("/api/rasPi/networks", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
