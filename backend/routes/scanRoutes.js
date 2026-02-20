2cf5a3086d6e38df495fe574ccd7fd455dcdeb45
cscendaya <carlieshaye24@gmail.com>
2026-02-20 10:42:43 +0800

    removed trigger scan  ; soon to omit whole scan route file

--- backend/routes/scanRoutes.js
+++ backend/routes/scanRoutes.js
@@ -3,23 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-// POST /api/rasPi_scan/trigger_scan from React --NOT USED!!!
-router.post('/trigger_scan', async (req, res) => {
-  try {
-    const { ssid } = req.body; //galing sa react
 
-    // Call FastAPI on the Pi
-    const response = await axios.post('http://mothership.tail781e52.ts.net:8000/api/rasPi_scan/trigger_scan', { ssid });
-
-    // Here you can save response.data to Supabase etc.
-    res.json(response.data);
-  } catch (err) {
-    console.error('FastAPI error:', err.message);
-    res.status(500).json({ error: 'Scan failed' });
-  }
-});
-
-module.exports = router; 
 
 //TESTING, PAG ITTOGGLE NA UNG ENABLE/DISABLE 
 
